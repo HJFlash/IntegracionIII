5d9b2f910498dc8537184b5963c5d664e0217eab
Carlos HJ <chuenumanj@gmail.com>
2024-08-31 17:18:04 -0400

    Pruebas iniciales con React Native y Android
    Se pudo probar la aplicacion mediante expo en Android y se hicieron cambios en una imagen y texto para probar

--- Frontend_movil/app/(tabs)/index.tsx
+++ Frontend_movil/app/(tabs)/index.tsx
@@ -11,16 +11,16 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/mike.png')}
           style={styles.reactLogo}
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Wena po ctm</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">Paso 1: Try it</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
