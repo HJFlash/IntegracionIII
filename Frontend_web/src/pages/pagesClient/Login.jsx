d139022887ba57b526e03c1629096b5c306e62ff
Igmacoi <ig.2001b@gmail.com>
2024-09-08 17:22:02 -0300

    Login/register
    Se termino el Login y se empezó el register

--- Frontend_web/src/pages/pagesClient/Login.jsx
+++ Frontend_web/src/pages/pagesClient/Login.jsx
@@ -1,43 +1,84 @@
 import React from 'react';
 import '../../styles/login.css';
-
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Login() {
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setShowPassword(!showPassword);
+  };
+
+
+
 
   return (
     <div className='container'>
       <div className='login'>
-        <div className='contentl'>
-          <h2>Login</h2>
-          <form id='formLogin' action="">
+        <div className='content01'>
+          <div className='regresar'>
+            <Link to="/" className='btn-inicio'>
+              Regresar al Inicio
+            </Link>
+          </div>
+          <div className='contentLog'>
+            <h2>Ingrese sus datos para <br /> Iniciar Sesion</h2>
+            <form id='formLogin' action="">
 
-            <div className='rutContainer'>
-              <div className='IconRed'></div>
-              <div className='rut'>
-                <label for="rut">Ingrese su RUT</label>
-                <input type="text" id="rut" name="rut" required/>
+              <div className='rutContainer'>
+                <div className='IconRed'></div>
+                <div className='rut'>
+                  <label for="rut">Ingrese su RUT</label>
+                  <input type="text" id="rut" name="rut" required/>
+                </div>
               </div>
-            </div>
 
-            <div className='contrasenaContainer'>
-              <div className='IconRed'></div>
-              <div className='contrasena'>
-                <label for="password">Ingrese su contraseña</label>
-                <input type="password" id="password" name="password" required/>
+              <div className='contrasenaContainer'>
+                <div className='IconRed'></div>
+                <div className='contrasena'>
+                  <label for="password">Ingrese su contraseña</label>
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    id="password"
+                    name="password"
+                    required
+                  />
+                </div>
               </div>
-            </div>
 
-            <div>
-              <input type="checkbox"/> Mostrar Contrasena
-            </div>
+              <div className='mostrarContrasena'>
+                <input 
+                  type="checkbox"
+                  id="mostrarPassword"
+                  className='checkboxContra'
+                  checked={showPassword}
+                  onChange={handleCheckboxChange}
+                  />
+                <p>Mostrar Contraseña</p>
+              </div>
 
-          </form>
+              <div className='btn-orange'>
+                <button type='submit'>Iniciar Sesión</button>
+              </div>
+            </form>
+          </div>
 
         </div>
       </div>
       
       <div className='initRegister'>
-        <p>a</p>
+        <div className='containerRegister'>
+          <p>Aun no tienes una cuenta? <br />
+          Solicita tu registro aqui</p>
+          <div className='btn-orange'>
+            <Link to="/Register">
+              <button>Solicitar Registro</button>
+            </Link>
+          </div>
+
+        </div>
       </div>
     </div>
   );
--- Frontend_web/src/pages/pagesClient/Register.jsx
+++ Frontend_web/src/pages/pagesClient/Register.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../styles/register.css';
 
 
 function Register() {
 
   return (
-    <div className='Register'>
-      <p>Registesssssssssr</p>
+    <div className='container'>
+
+      <div className='initlogin'>
+        <div className='containerInitL'> 
+          <div className='regresar1'>
+            <Link to="/" className='btn-inicio1'>
+              Regresar al Inicio
+            </Link>
+          </div>
+          <div className='infoSoli1'>
+            <p>Ya Tienes una cuenta?</p>
+            <p>Es necesario Iniciar Sesión <br /> para realizar una solicitud</p>
+          </div>
+          <div className='btn-orange1'>
+            <Link to="/Login">
+              <button>Ir a Iniciar Sesión</button>
+            </Link>
+          </div>
+          
+        </div>
+      </div>
+
+
+      <div className='Register'>
+
+      </div>
     </div>
 
   );
