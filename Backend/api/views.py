90f90fc43f21f9669eb840bb0797dd1b480b9ce7
rodrigo <rgutierrezvielma@gmail.com>
2024-09-22 02:30:18 -0300

    cambios de mensajes

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -8,30 +8,30 @@ import json
 def registro(request):
     if request.method == 'POST':
         datos = json.loads(request.body)
-        username = datos.get('username')
-        password = datos.get('password')
-        email = datos.get('email')
+        username = datos.get('Nombre')
+        password = datos.get('Contraseña')
+        email = datos.get('Email')
         if username and password and email:
             if User.objects.filter(username=username).exists():
-                return JsonResponse({'error': 'Username already exists'}, status=400)
+                return JsonResponse({'error': 'El nombre de usuario ya existe'}, status=400)
             usuario = User.objects.create_user(username=username, password=password, email=email)
-            return JsonResponse({'message': 'User created successfully'}, status=201)
-        return JsonResponse({'error': 'Invalid data'}, status=400)
+            return JsonResponse({'message': 'Usuario credo exitosamente'}, status=201)
+        return JsonResponse({'error': 'Datos invalidos'}, status=400)
 
 @csrf_exempt
 def login_vista(request):
     if request.method == 'POST':
         datos = json.loads(request.body)
-        username = datos.get('username')
-        password = datos.get('password')
+        username = datos.get('Nombre')
+        password = datos.get('Contraseña')
         usuario = authenticate(username=username, password=password)
         if usuario is not None and usuario.is_active:
             login(request, usuario)
-            return JsonResponse({'message': 'Logged in successfully'})
-        return JsonResponse({'error': 'Invalid credentials'}, status=401)
+            return JsonResponse({'message': 'Inicio de sesion exitoso'})
+        return JsonResponse({'error': 'Credenciales Invalidas'}, status=401)
 
 @csrf_exempt
 def logout_vista(request):
     if request.method == 'POST':
         logout(request)
-        return JsonResponse({'message': 'Logged out successfully'})
+        return JsonResponse({'message': 'Desconexion exitosa'})
