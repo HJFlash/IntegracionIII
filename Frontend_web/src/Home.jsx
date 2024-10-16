b80ed6b632a39f6cd24a76caa75695cccecfe052
Igmacoi <ig.2001b@gmail.com>
2024-10-16 19:16:02 -0300

    Corrección errores de consola del navegador
    Se corrigieron errores del video que se muestra en el inicio, existen errores en consola producto de extensiones como bloqueadores de anuncios entre otras extensiones.

--- Frontend_web/src/Home.jsx
+++ Frontend_web/src/Home.jsx
@@ -52,10 +52,10 @@ function Home() {
             height="315" 
             src="https://www.youtube.com/embed/Y-x0efG1seA?si=Bw2I4bQeYNd_s8jU" 
             title="YouTube video player" 
-            frameborder="0" 
+            frameBorder="0" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            referrerpolicy="strict-origin-when-cross-origin" 
-            allowfullscreen>
+            referrerPolicy="strict-origin-when-cross-origin" 
+            allowFullScreen>
           </iframe>
         </div>
       </div>
