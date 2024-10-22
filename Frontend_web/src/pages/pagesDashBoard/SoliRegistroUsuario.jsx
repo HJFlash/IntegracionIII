45c6ac55af9e9a5d56edc6993a7adfa578397399
Igmacoi <ig.2001b@gmail.com>
2024-10-21 21:00:46 -0300

    Corrección solicitudes de registro
    Modificación crud para el registro y que muestre los nuevos datos

--- Frontend_web/src/pages/pagesDashBoard/SoliRegistroUsuario.jsx
+++ Frontend_web/src/pages/pagesDashBoard/SoliRegistroUsuario.jsx
@@ -19,18 +19,20 @@ const SoliRegistroUsuario = () => {
 
   return (
     <div>
-        <div className='max-w-6xl mx-auto p-6 bg-white shadow-lg rounded-lg'>
+        <div className='max-w-max mx-auto p-6 bg-white shadow-lg rounded-lg'>
             <table className='min-w-full table-auto'>
                 <thead>
                     <tr className='bg-gray-200'>
-                        <th className='px-6 py-3 text-left text-gray-700 font-semibold'>Nombre</th>
-                        <th className='px-6 py-3 text-left text-gray-700 font-semibold'>Apellido</th>
+                        <th className='px-6 py-3 text-left text-gray-700 font-semibold'>1°Nombre</th>
+                        <th className='px-6 py-3 text-left text-gray-700 font-semibold'>2°Nombre</th>
+                        <th className='px-6 py-3 text-left text-gray-700 font-semibold'>1°Apellido</th>
+                        <th className='px-6 py-3 text-left text-gray-700 font-semibold'>2°Apellido</th>
                          <th className='px-6 py-3 text-left text-gray-700 font-semibold'>RUT</th>
                          <th className='px-6 py-3 text-left text-gray-700 font-semibold'>contacto</th>
                          <th className='px-6 py-3 text-left text-gray-700 font-semibold'>calle</th>
                          <th className='px-6 py-3 text-left text-gray-700 font-semibold'>num_apar </th>
                          <th className='px-6 py-3 text-left text-gray-700 font-semibold'>num_casa </th>
-                         <th className='px-6 py-3 text-left text-gray-700 font-semibold'>Estatus Solicitud</th>
+                         <th className='px-6 py-3 text-left text-gray-700 font-semibold'>tipo usuario</th>
 
                     </tr>
                 </thead>
@@ -38,8 +40,10 @@ const SoliRegistroUsuario = () => {
                     {data.length > 0? (
                     data.map((item, index) => (
                         <tr className='border-b' key={index}>
-                            <td className='px-2 py-1 text-gray-950'>{item.nombres}</td>
-                            <td className='px-2 py-1 text-gray-950'>{item.apellidos}</td>
+                            <td className='px-2 py-1 text-gray-950'>{item.primer_nombre}</td>
+                            <td className='px-2 py-1 text-gray-950'>{item.segundo_nombre}</td>
+                            <td className='px-2 py-1 text-gray-950'>{item.primer_apellido}</td>
+                            <td className='px-2 py-1 text-gray-950'>{item.segundo_apellido}</td>
                             <td className='px-2 py-1 text-gray-950'>{item.rut}</td>
                             <td className='px-2 py-1 text-gray-950'>{item.contacto}</td>
                             <td className='px-2 py-1 text-gray-950'>{item.calle}</td>
